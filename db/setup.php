a23fb9f7442a613585a653b8e92af6932b377957
Dennis <dennis.van.meel@freshheads.com>
2014-12-22 10:54:38 +0100

    Setup script

--- db/setup.php
+++ db/setup.php
@@ -1,1 +1,17 @@
 <?php
+
+$connection = new SQLite3('bamidb');
+
+try {
+    $connection->query('DROP TABLE IF EXISTS USERS;');
+    $connection->query('DROP TABLE IF EXISTS ORDERS;');
+    $connection->query('DROP TABLE IF EXISTS TAGS;');
+
+    $connection->query(file_get_contents('sql/users.sql'));
+    $connection->query(file_get_contents('sql/orders.sql'));
+    $connection->query(file_get_contents('sql/tags.sql'));
+} catch (Exception $e) {
+    echo 'Failed: ' . $e;
+}
+
+echo 'Success';
